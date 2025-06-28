56aee0aafddaf78a082b514d0b9b2ce621c7ee34
Hirotsune0214 <hiroyukitsune4@gmail.com>
2025-06-28 11:30:43 +0900

    ✨ Add Read more/less button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
 import "./Header.css";
 
-const Header = ({ onSearch, onClickUpcoming }) => {
+const Header = ({ onSearch, onClickUpcoming, onClickPopular }) => {
   const [searchText, setSearchText] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,11 +13,22 @@ const Header = ({ onSearch, onClickUpcoming }) => {
     <>
       <Navbar className="bg-secondary">
         <Container className="justify-content-start  bg-secondary">
-          <Navbar.Brand href="/" className="text-white ">
+          <Navbar.Brand href="/" className="categoryChoice">
             🎬MovieDb App
           </Navbar.Brand>
-          <Nav.Link href="#" className="text-white" onClick={onClickUpcoming}>
+          <Nav.Link
+            href="#"
+            className="me-3 categoryChoice"
+            onClick={onClickUpcoming}
+          >
             Upcoming
+          </Nav.Link>
+          <Nav.Link
+            href="#"
+            className="categoryChoice"
+            onClick={onClickPopular}
+          >
+            Popular
           </Nav.Link>
           <Form className="d-flex ms-auto" onSubmit={handleSubmit}>
             <Form.Control
--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,11 @@
 // 途中で変更されない・共通で使いたい値を格納
 
-export const API_URL =
+export const API_POPULAR =
   "https://api.themoviedb.org/3/movie/popular?api_key=bb46848237eacc0a36827f6639b47ee3";
 export const API_IMG = "https://image.tmdb.org/t/p/w500/";
 export const API_SEARCH =
   "https://api.themoviedb.org/3/search/movie?api_key=bb46848237eacc0a36827f6639b47ee3&query=";
 export const API_UPCOMING =
   "https://api.themoviedb.org/3/movie/upcoming?api_key=bb46848237eacc0a36827f6639b47ee3&language=en-US&page=1";
+export const API_NOWPLAYING =
+  "https://api.themoviedb.org/3/movie/now_playing?api_key=bb46848237eacc0a36827f6639b47ee3&language=en-US&page=1";
